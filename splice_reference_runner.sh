ead3c91346701cc4d496f4a8bff11e406254a49e
Sami Ul Haq <137592251+sulhaq97@users.noreply.github.com>
2024-07-09 15:12:04 -0400

    Update splice_reference_runner.sh

--- splice_reference_runner.sh
+++ splice_reference_runner.sh
@@ -6,6 +6,8 @@
 #SBATCH -N 1
 #SBATCH -o %x-%j.out
 
-module load R/4.2.1
+# This is the script for running the reference generation R script
+# This script runs on a slurm high-performance computing cluster with the above parameters (recommended)
 
+module load R/4.2.1
 Rscript splicewiz_reference_generation.R
